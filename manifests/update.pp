5fa1d418a555338c4fe8b709029a90e040080d1d
Philippe Beaumont <philippe.beaumont@nfrance.com>
2023-03-02 16:02:13 +0100

    import code from PR 1073

--- manifests/update.pp
+++ manifests/update.pp
@@ -5,7 +5,7 @@
 class apt::update {
   assert_private()
 
-  #TODO: to catch if apt_update_last_success has the value of -1 here. If we
+  #TODO: to catch if $apt_update_last_success has the value of -1 here. If we
   #opt to do this, a info/warn would likely be all you'd need likely to happen
   #on the first run, but if it's not run in awhile something is likely borked
   #with apt and we'd want to know about it.
@@ -18,8 +18,8 @@ class apt::update {
       #compare current date with the apt_update_last_success fact to determine
       #if we should kick apt_update.
       $daily_threshold = (Integer(Timestamp().strftime('%s')) - 86400)
-      if $facts['apt_update_last_success'] {
-        if $facts['apt_update_last_success'] + 0 < $daily_threshold {
+      if $apt::apt_update_last_success {
+        if $apt::apt_update_last_success + 0 < $daily_threshold {
           $_kick_apt = true
         } else {
           $_kick_apt = false
@@ -33,8 +33,8 @@ class apt::update {
       #compare current date with the apt_update_last_success fact to determine
       #if we should kick apt_update.
       $weekly_threshold = (Integer(Timestamp().strftime('%s')) - 604800)
-      if $facts['apt_update_last_success'] {
-        if $facts['apt_update_last_success'] + 0 < $weekly_threshold {
+      if $apt::apt_update_last_success {
+        if ( $apt::apt_update_last_success + 0 < $weekly_threshold ) {
           $_kick_apt = true
         } else {
           $_kick_apt = false
@@ -56,13 +56,19 @@ class apt::update {
   } else {
     $_refresh = true
   }
+  # We perform the update in an `unless` clause of the exec, and
+  # return true only if the `/var/cache/apt/pkgcache.bin` file changed.
+  # This ensures that Puppet does not report a change if the
+  # update command had no effect. See MODULES-10763 for discussion.
   exec { 'apt_update':
-    command     => "${apt::provider} update",
+    command     => "echo ${apt::provider} updated the package cache.",
     loglevel    => $apt::_update['loglevel'],
     logoutput   => 'on_failure',
+    path        => '/bin:/usr/bin',
+    provider    => shell,
     refreshonly => $_refresh,
     timeout     => $apt::_update['timeout'],
     tries       => $apt::_update['tries'],
-    try_sleep   => 1,
+    unless      => epp('apt/update_had_no_effect.sh.epp'),
   }
 }
