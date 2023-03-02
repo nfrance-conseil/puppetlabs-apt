addd60f9b1ca6357de9a4dc0469c27eec2f46169
Philippe Beaumont <philippe.beaumont@nfrance.com>
2023-03-02 16:02:13 +0100

    import code from PR 1073

--- spec/acceptance/apt_backports_spec.rb
+++ spec/acceptance/apt_backports_spec.rb
@@ -3,6 +3,21 @@
 require 'spec_helper_acceptance'
 
 describe 'apt::backports' do
+  context 'update_had_no_effect script' do
+    let(:pp) do
+      <<-MANIFEST
+        exec { 'Test the script':
+          command   => epp('apt/update_had_no_effect.sh.epp'),
+          logoutput => true,
+          returns   => [-1],
+        }
+      MANIFEST
+    end
+
+    it 'produces output' do
+      idempotent_apply(pp)
+    end
+  end
   context 'when using defaults' do
     let(:pp) do
       <<-MANIFEST
